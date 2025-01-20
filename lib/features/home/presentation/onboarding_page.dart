775d5d8c1e682a995841a55d2353538a7bd61d3e
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-01-19 22:38:48 -0500

    feat:added new styles for onboarding_page.dart

--- lib/features/home/presentation/onboarding_page.dart
+++ lib/features/home/presentation/onboarding_page.dart
@@ -7,82 +7,98 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryBlue, // Usamos el color que definimos en app_colors.dart
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 16.0),
-              // Imagen en la parte superior
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: Image.asset(
-                    'assets/images/font_3.png',
-                    fit: BoxFit.contain,
-                    height: 400,
-                    width: 400,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 1.0),
-              // Texto principal
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: Text(
-                    'Control everything  anywhere',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      fontSize: 48.0, // Tamaño más grande para asemejar la imagen
-                      fontWeight: FontWeight.w800, // Peso más grueso
-                      color: Colors.white,
-                      fontFamily: 'OpenSans',
-
-                      height: 1.4, // Espaciado entre líneas
-                      letterSpacing: 0.5, // Espaciado entre letras
+      // Aplicamos el gradiente al fondo
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: AppColors.customGradient, // Gradiente definido
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 16.0),
+                // Imagen en la parte superior con bordes redondeados
+                Expanded(
+                  flex: 2,
+                  child: Center(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0), // Márgenes a los lados
+                      height: 400,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0), // Bordes redondeados
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 10,
+                            offset: const Offset(0, 5), // Sombra hacia abajo
+                          ),
+                        ],
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/img_fount.png'),
+                          fit: BoxFit.cover, // Ajusta la imagen al contenedor
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16.0),
-              // Botón principal
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/login'); // Asume que tenemos una ruta para Login
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: AppColors.primaryBlue, // Texto en azul
-                  backgroundColor: Colors.white, // Fondo blanco
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                ),
-                child: const Text(
-                  'Get started',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-
+                const SizedBox(height: 1.0),
+                // Texto principal
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Text(
+                      'Control everything  anywhere',
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        fontSize: 48.0, // Tamaño más grande para asemejar la imagen
+                        fontWeight: FontWeight.w800, // Peso más grueso
+                        color: Colors.white,
+                        fontFamily: 'OpenSans',
+                        height: 1.4, // Espaciado entre líneas
+                        letterSpacing: 0.5, // Espaciado entre letras
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 30.0),
-              // Botón de texto
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/login'); // Navegar al login
-                },
-                child: const Text(
-                  'Already have an account? Log in',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14.0, // Ajuste del tamaño de fuente
-                    fontWeight: FontWeight.w500, // Peso del texto
+                const SizedBox(height: 16.0),
+                // Botón principal
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login'); // Asume que tenemos una ruta para Login
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: const Color(0xFF2E1C9C), // Texto en azul oscuro del gradiente
+                    backgroundColor: Colors.white, // Fondo blanco
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  child: const Text(
+                    'Get started',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 30.0),
+                // Botón de texto
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login'); // Navegar al login
+                  },
+                  child: const Text(
+                    'Already have an account? Log in',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.0, // Ajuste del tamaño de fuente
+                      fontWeight: FontWeight.w500, // Peso del texto
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
