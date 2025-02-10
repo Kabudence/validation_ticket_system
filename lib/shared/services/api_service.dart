8019ce8a27597392ad19e4bfd271e53415aef736
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-02-10 14:03:11 -0500

    feat:added some changess

--- lib/shared/services/api_service.dart
+++ lib/shared/services/api_service.dart
@@ -4,5 +4,7 @@ class Api {
   static const String baseUrl = "http://10.0.2.2:5000/api"; // Cambiar solo aquí cuando cambie el URL base
   static final Dio dio = Dio(BaseOptions(
     baseUrl: baseUrl,
+    connectTimeout: Duration(milliseconds: 90000),
+    receiveTimeout: Duration(milliseconds: 90000),
   ));
 }
