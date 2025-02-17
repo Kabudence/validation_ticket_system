a0455f889e760b37161fa32ca1ae496c5721da19
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-02-17 11:56:01 -0500

    feat:added new route with the backend deployment

--- lib/features/boletas/presentation/pages/boletas_page.dart
+++ lib/features/boletas/presentation/pages/boletas_page.dart
@@ -12,24 +12,31 @@ class BoletasPage extends StatefulWidget {
 
 class _BoletasPageState extends State<BoletasPage> {
   final BoletasService boletasService = BoletasService();
-
-  // Guardamos el future para reusarlo en el FutureBuilder
   late Future<List<dynamic>> _futureBoletas;
 
   @override
   void initState() {
     super.initState();
-    // Cargar la lista de boletas inicialmente
     _loadBoletas();
   }
 
-  // Método para cargar o recargar las boletas
+  /// Método para cargar o recargar la lista de boletas
   void _loadBoletas() {
-    // Llama a tu endpoint /ventas/today-inprocess-peru con limit y offset
     _futureBoletas = boletasService.getBoletasTodayInProcessPeru(
       limit: 10,
       offset: 0,
     );
+
+    // Asegurar que `setState()` solo se llame si el widget sigue montado
+    _futureBoletas.then((_) {
+      if (mounted) {
+        setState(() {}); // Solo actualizar si el widget sigue en pantalla
+      }
+    }).catchError((error) {
+      if (mounted) {
+        setState(() {}); // Manejar el error solo si el widget sigue activo
+      }
+    });
   }
 
   @override
@@ -38,137 +45,139 @@ class _BoletasPageState extends State<BoletasPage> {
       appBar: AppBar(
         title: const Text(
           'Boletas',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.transparent,
         elevation: 0,
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh, color: Colors.white),
+            onPressed: _loadBoletas, // Botón para actualizar manualmente
+            tooltip: 'Actualizar',
+          ),
+        ],
       ),
-      extendBodyBehindAppBar: true, // Para que el AppBar quede sobre el gradiente
+      extendBodyBehindAppBar: true,
       body: Container(
         decoration: const BoxDecoration(
-          gradient: AppColors.customGradient, // Gradiente de tu tema
+          gradient: AppColors.customGradient,
         ),
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: FutureBuilder<List<dynamic>>(
-              // Usamos el future que tenemos en la variable
-              future: _futureBoletas,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text(
-                      'Error: ${snapshot.error}',
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  );
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(
-                    child: Text(
-                      'No hay boletas disponibles',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                  );
-                } else {
-                  final boletas = snapshot.data!;
-                  return ListView.builder(
-                    itemCount: boletas.length,
-                    itemBuilder: (context, index) {
-                      final boleta = boletas[index];
+          child: RefreshIndicator(
+            onRefresh: () async {
+              _loadBoletas();
+              await _futureBoletas;
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FutureBuilder<List<dynamic>>(
+                future: _futureBoletas,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        'Error: ${snapshot.error}',
+                        style: const TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        'No hay boletas disponibles',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    );
+                  } else {
+                    final boletas = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: boletas.length,
+                      itemBuilder: (context, index) {
+                        final boleta = boletas[index];
 
-                      // Opcional: Imprimir para depurar
-                      // print("---- boleta (index=$index): $boleta");
-
-                      return Card(
-                        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        color: Colors.white.withOpacity(0.9),
-                        elevation: 6,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Número: ${boleta['num_docum']}',
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                'Cliente: ${boleta['cliente']}',
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                              const SizedBox(height: 8),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Estado: ${boleta['estado']}',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: boleta['estado'] == 'EN PROCESO'
-                                          ? Colors.red
-                                          : Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                        return Card(
+                          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                          color: Colors.white.withOpacity(0.9),
+                          elevation: 6,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Número: ${boleta['num_docum']}',
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  ElevatedButton(
-                                    onPressed: () async {
-                                      // Tomamos idmov y num_docum
-                                      final int idmov = boleta['idmov'] ?? 0;
-                                      final String numDocum = boleta['num_docum'] ?? "Sin doc";
-
-                                      // Navegamos a CompletarVentaPage y esperamos el resultado
-                                      final result = await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => CompletarVentaPage(
-                                            idmov: idmov,
-                                            numDocum: numDocum,
-                                          ),
-                                        ),
-                                      );
-
-                                      // Si la página de completar venta devolvió true, recargamos boletas
-                                      if (result == true) {
-                                        setState(() {
-                                          _loadBoletas();
-                                        });
-                                      }
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: AppColors.secondBlue,
-                                      foregroundColor: Colors.white,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(12),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  'Cliente: ${boleta['cliente']}',
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Estado: ${boleta['estado']}',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: boleta['estado'] == 'EN PROCESO'
+                                            ? Colors.red
+                                            : Colors.white,
+                                        fontWeight: FontWeight.bold,
                                       ),
                                     ),
-                                    child: const Text('Completar venta'),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                    ElevatedButton(
+                                      onPressed: () async {
+                                        final int idmov = boleta['idmov'] ?? 0;
+                                        final String numDocum = boleta['num_docum'] ?? "Sin doc";
+
+                                        final result = await Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => CompletarVentaPage(
+                                              idmov: idmov,
+                                              numDocum: numDocum,
+                                            ),
+                                          ),
+                                        );
+
+                                        // Si se completó la venta, recargamos boletas manualmente
+                                        if (result == true) {
+                                          _loadBoletas();
+                                        }
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: AppColors.secondBlue,
+                                        foregroundColor: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(12),
+                                        ),
+                                      ),
+                                      child: const Text('Completar venta'),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
             ),
           ),
         ),
