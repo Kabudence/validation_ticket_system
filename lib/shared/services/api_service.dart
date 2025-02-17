991d7ee86114adb3f8aefad97dae4a526eca54ae
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-02-17 11:55:56 -0500

    feat:added new route with the backend deployment

--- lib/shared/services/api_service.dart
+++ lib/shared/services/api_service.dart
@@ -1,10 +1,27 @@
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Api {
-  static const String baseUrl = "http://10.0.2.2:5000/api"; // Cambiar solo aquí cuando cambie el URL base
+  static const String baseUrl = "https://web-production-927a.up.railway.app/api"; // Cambia el URL base si es necesario
+
   static final Dio dio = Dio(BaseOptions(
     baseUrl: baseUrl,
-    connectTimeout: Duration(milliseconds: 90000),
-    receiveTimeout: Duration(milliseconds: 90000),
-  ));
+    connectTimeout: const Duration(milliseconds: 90000),
+    receiveTimeout: const Duration(milliseconds: 90000),
+  ))
+    ..interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        // Recuperar el token de SharedPreferences
+        final prefs = await SharedPreferences.getInstance();
+        final token = prefs.getString('authToken');
+
+        if (token != null) {
+          // Agregar el header Authorization con el formato Bearer
+          options.headers["Authorization"] = "Bearer $token";
+        }
+
+        return handler.next(options);
+      },
+      // Puedes agregar onResponse y onError según necesites
+    ));
 }
